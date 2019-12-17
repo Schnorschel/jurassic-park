ca18adec3cdd86a9c3a62abf0a562eedf7d3c5a4
Georg Oehl <oehl@pobox.com>
2019-12-16 22:20:51 -0500

    Explorer Mode

---
+++ JurassicParkContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace jurassic_park
+{
+  public partial class JurassicParkContext : DbContext
+  {
+    public DbSet<Dinosaur> Dinosaurs { get; set; }
+
+    public JurassicParkContext()
+    {
+    }
+
+    public JurassicParkContext(DbContextOptions<JurassicParkContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        optionsBuilder.UseNpgsql("server=localhost;database=jurassic_park;user id=postgres;password=");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace jurassic_park
 {
   class Program
   {
-    static List<Dinosaur> JurassicPark = new List<Dinosaur>();
+    // static List<Dinosaur> JurassicPark = new List<Dinosaur>();
+    static JurassicParkContext Db = new JurassicParkContext();
+    static string[] babyNames = new string[] { "Junior", "Bashful", "Happy" };
+    static string[] diet = new string[] { "herbivore", "carnivore" };
 
     static void ViewAll()
     {
       Console.WriteLine("Jurassic Park currently has the following inhabitants:");
       Console.WriteLine("------------------------------------------------------");
 
-      DisplayListOfDinos(JurassicPark.OrderBy(dino => dino.DateAcquired));
+      DisplayListOfDinos(Db.Dinosaurs.OrderBy(dino => dino.DateAcquired));
     }
 
     static void AddDino()
@@ -33,35 +38,37 @@ namespace jurassic_park
       dino.Weight = int.Parse(weight);
       dino.EnclosureNumber = int.Parse(enclosure);
 
-      JurassicPark.Add(dino);
+      Db.Dinosaurs.Add(dino);
+      Db.SaveChanges();
     }
 
     static void RemoveDino()
     {
       Console.Write("What's the dinosaur's name you want to remove? ");
       string name = Console.ReadLine();
-      var RemoveDino = JurassicPark.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
+      var RemoveDino = Db.Dinosaurs.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
       while (RemoveDino == null && name != "c")
       {
         Console.Write($"{name} was not found in Jurassic Park. Try again or (c)ancel: "); name = Console.ReadLine();
-        RemoveDino = JurassicPark.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
+        RemoveDino = Db.Dinosaurs.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
       }
       if (name == "c") { return; }
-      JurassicPark.Remove(RemoveDino);
+      Db.Dinosaurs.Remove(RemoveDino);
+      Db.SaveChanges();
       Console.WriteLine($"{name} was successfully removed from Jurassic Park.");
     }
 
     static void TransferDino()
     {
       Console.Write("What's the dinosaur's name you want to transfer? "); string name = Console.ReadLine();
-      var MoveDino = JurassicPark.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
+      var MoveDino = Db.Dinosaurs.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
       while (MoveDino == null && name != "c")
       {
         Console.Write($"{name} was not found in Jurassic Park. Try again or (c)ancel: "); name = Console.ReadLine();
-        MoveDino = JurassicPark.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
+        MoveDino = Db.Dinosaurs.FirstOrDefault(dino => dino.Name.ToLower() == name.ToLower());
       }
       if (name == "c") { return; }
-      Console.WriteLine($"{name} is currently in enclosure {MoveDino.EnclosureNumber}.");
+      Console.WriteLine($"{name} currently lives in " + (MoveDino.EnclosureNumber == -1 ? "the wild" : $"enclosure {MoveDino.EnclosureNumber}") + ".");
       Console.Write($"Which enclosure do you want to move {name} to? "); var enclosure = Console.ReadLine();
       int i;
       while (!int.TryParse(enclosure, out i))
@@ -69,6 +76,7 @@ namespace jurassic_park
         Console.Write($"{enclosure} is not a valid number. Try again: "); enclosure = Console.ReadLine();
       }
       MoveDino.EnclosureNumber = int.Parse(enclosure);
+      Db.SaveChanges();
       Console.WriteLine($"{name} was successfully moved to enclosure {enclosure}.");
     }
 
@@ -77,20 +85,77 @@ namespace jurassic_park
       Console.WriteLine("Jurassic Park's top three dinosaurs by weight are:");
       Console.WriteLine("--------------------------------------------------");
 
-      DisplayListOfDinos(JurassicPark.OrderByDescending(dino => dino.Weight).Take(3));
+      DisplayListOfDinos(Db.Dinosaurs.OrderByDescending(dino => dino.Weight).Take(3));
 
-      Console.WriteLine("--------------------------------------------------");
+      // Console.WriteLine("--------------------------------------------------");
     }
 
     static void ViewDiet()
     {
       Console.WriteLine("Jurassic Park's dinosaurs' diet is as follows:");
       Console.WriteLine("----------------------------------------------");
-      foreach (var dino in JurassicPark)
+      foreach (var dino in Db.Dinosaurs)
       {
         Console.WriteLine($"{dino.Name} is a {dino.DietType}.");
       }
       Console.WriteLine("----------------------------------------------");
+    }
+
+    static void ReleaseFromEnclosure()
+    {
+      Console.Write("Which enclosure do you want to open for release? "); string enclosure = Console.ReadLine();
+      int numReleased = 0;
+      string releasedDinos = "";
+
+      foreach (var dino in Db.Dinosaurs)
+      {
+        if (dino.EnclosureNumber == int.Parse(enclosure))
+        {
+          numReleased++;
+          releasedDinos = releasedDinos + (releasedDinos.Length > 0 ? ", " : "") + dino.Name;
+          dino.EnclosureNumber = -1;
+        }
+      }
+      if (releasedDinos.Length > 0)
+      {
+        Db.SaveChanges();
+        Console.WriteLine($"The following {numReleased} dinosaur" + plurify(numReleased) + " " + wasWere(numReleased) + $" released into the wild from closure {enclosure}:");
+        Console.WriteLine(releasedDinos);
+      }
+      else
+      {
+        Console.WriteLine($"There are currently no dinosaurs in enclosure {enclosure}");
+      }
+
+    }
+
+    static void HatchDino()
+    {
+
+      Random rnd = new Random();
+      string randomName = babyNames[rnd.Next(0, 3)];
+      int randomWeight = rnd.Next(200, 1501);
+      string randomDiet = diet[rnd.Next(0, 2)];
+
+      Dinosaur babyDino = new Dinosaur();
+
+      babyDino.Name = randomName;
+      babyDino.Weight = randomWeight;
+      babyDino.DietType = randomDiet;
+      babyDino.DateAcquired = DateTime.Today;
+      babyDino.EnclosureNumber = rnd.Next(1, 11);
+
+      Console.WriteLine($"{babyDino.Name}, a {babyDino.DietType}, weighing in at {babyDino.Weight} lbs, was born in enclosure {babyDino.EnclosureNumber}.");
+      Console.WriteLine("Congratulate the proud parents!");
+      Db.Add(babyDino);
+      Db.SaveChanges();
+    }
+
+    static void NeedsASheep()
+    {
+      Console.WriteLine("Someone is a little lean today. The following fella could use a sheep:");
+      DisplayListOfDinos(Db.Dinosaurs.Where(dino => dino.DietType == "carnivore").OrderBy(dino => dino.Weight).Take(1));
+
     }
 
     static void Quit()
@@ -100,27 +165,44 @@ namespace jurassic_park
 
     static void DisplayListOfDinos(IEnumerable<Dinosaur> dinoList)
     {
+      int i = 0;
+
       foreach (var dino in dinoList)
       {
-        if (dinoList.First() != dino)
+        int dinoNameLength = dino.Name.Length;
+        if (i++ > 0)
         {
-          Console.WriteLine(new string('-', dino.Name.Length));
+          Console.WriteLine(new string('-', dinoNameLength));
         }
         Console.Write($"{dino.Name}"); Console.WriteLine(", since " + dino.DateAcquired.ToString("M/dd/yyyy"));
         Console.WriteLine(new string('-', dino.Name.Length + dino.DateAcquired.ToString("M/dd/yyyy").Length + 8));
-        Console.WriteLine($"  is a {dino.DietType},");
+        Console.WriteLine($"  ..is a {dino.DietType},");
         Console.WriteLine("  weighs " + dino.Weight.ToString() + " lbs,");
-        Console.WriteLine($"  lives in enclosure {dino.EnclosureNumber}.");
-        if (dinoList.Last() == dino)
-        {
-          Console.WriteLine("------------------------------------------------------");
-        }
+        Console.WriteLine("  lives in " + (dino.EnclosureNumber == -1 ? "the wild" : $"enclosure {dino.EnclosureNumber}") + ".");
       }
+      Console.WriteLine("------------------------------------------------------");
+    }
+
+    static string plurify(int num)
+    {
+      return (num == 1) ? "" : "s";
+    }
+
+    static string wasWere(int num)
+    {
+      return (num == 1) ? "was" : "were";
     }
 
     static void Main(string[] args)
     {
       ConsoleKeyInfo userResponse;
+
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+      IConfigurationRoot configuration = builder.Build();
+
       Console.WriteLine("Welcome to Jurassic Park!");
       do
       {
@@ -128,14 +210,17 @@ namespace jurassic_park
         Console.WriteLine("                      (A)dd a dinosaur");
         Console.WriteLine("                      (R)emove a dinosaur");
         Console.WriteLine("                      (T)ransfer a dinosaur");
+        Console.WriteLine("                 Relea(s)e dinosaurs from enclosure");
+        Console.WriteLine("                      (H)atch a new dinosaur");
         Console.WriteLine("                 View (b)iggest dinosaurs");
+        Console.WriteLine("              Someone (N)eeds a sheep");
         Console.WriteLine("  View all dinosaurs' (d)iet");
         Console.WriteLine("                      (V)iew all dinosaurs");
         Console.WriteLine("                      (Q)uit");
 
         Console.Write("What would you like to do? "); userResponse = Console.ReadKey();
         Console.WriteLine();
-        while ("artbdvq".IndexOf(userResponse.Key.ToString().ToLower()) < 0)
+        while ("artshnbdvq".IndexOf(userResponse.Key.ToString().ToLower()) < 0)
         {
           Console.Write("That's not an accepted response. Try again: "); userResponse = Console.ReadKey();
           Console.WriteLine();
@@ -155,11 +240,20 @@ namespace jurassic_park
           case "b":
             ViewBigDinos();
             break;
+          case "n":
+            NeedsASheep();
+            break;
+          case "h":
+            HatchDino();
+            break;
           case "r":
             RemoveDino();
             break;
           case "t":
             TransferDino();
+            break;
+          case "s":
+            ReleaseFromEnclosure();
             break;
           case "q":
             Quit();
